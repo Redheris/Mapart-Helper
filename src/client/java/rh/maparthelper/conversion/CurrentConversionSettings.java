ad900878203f36226e992537550685fd03768feb
Anatoly Solomonov <solomonov010@mail.ru>
2025-06-08 19:25:30 +0400

    Added fields of NativeImageBeckedTexture and Identifier for working with mapart image

--- src/client/java/rh/maparthelper/conversion/CurrentConversionSettings.java
+++ src/client/java/rh/maparthelper/conversion/CurrentConversionSettings.java
@@ -1,6 +1,14 @@
 package rh.maparthelper.conversion;
 
+import net.minecraft.client.texture.NativeImageBackedTexture;
+import net.minecraft.util.Identifier;
+import rh.maparthelper.MapartHelper;
+
 public class CurrentConversionSettings {
+    // GUI image
+    public static NativeImageBackedTexture guiMapartImage = new NativeImageBackedTexture("mapart_gui_texture", 128, 128, true);
+    public static Identifier guiMapartId = Identifier.of(MapartHelper.MOD_ID, "mapart_gui_texture");
+
     // Map settings
     public static String mapartName = "New mapart";
     public static int width = 1;
