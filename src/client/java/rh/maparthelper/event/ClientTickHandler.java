6a079cf519f5042b968a2009cacc15987c230c86
TheEnderCore <theendercore@gmail.com>
2025-10-08 17:23:34 +0300

    WorldRenderEvents have been temporary removed since 0.133.11+1.21.9

--- src/client/java/rh/maparthelper/event/ClientTickHandler.java
+++ src/client/java/rh/maparthelper/event/ClientTickHandler.java
@@ -2,20 +2,13 @@ package rh.maparthelper.event;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.block.entity.BeaconBlockEntityRenderer;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.util.math.Vec3d;
-import org.joml.Vector2i;
 import org.lwjgl.glfw.GLFW;
 import rh.maparthelper.MapartHelper;
 import rh.maparthelper.command.ClientCommandsContext;
 import rh.maparthelper.command.FakeMapsPreview;
 import rh.maparthelper.gui.MapartEditorScreen;
-import rh.maparthelper.util.MapUtils;
 
 public class ClientTickHandler {
     public static void init() {
@@ -38,7 +31,7 @@ public class ClientTickHandler {
             }
         });
 
-        WorldRenderEvents.AFTER_ENTITIES.register(context -> {
+       /* WorldRenderEvents.AFTER_ENTITIES.register(context -> {
             if (!ClientCommandsContext.showMapartStartPos()) return;
             MatrixStack matrices = context.matrixStack();
             VertexConsumerProvider vertexConsumers = context.consumers();
@@ -64,6 +57,6 @@ public class ClientTickHandler {
                     matrices.pop();
                 }
             }
-        });
+        });*/
     }
 }
