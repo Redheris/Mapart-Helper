ed979e643e0d5cf1b4229b74b79ba590ed3bec16
Anatoly Solomonov <solomonov010@mail.ru>
2025-11-04 11:53:32 +0400

    Refactored DecorativeButtonWidget - Removed inheritance from ButtonWidget - Text message can be replaced by an Identifier texture

--- src/client/java/rh/maparthelper/gui/widget/DecorativeButtonWidget.java
+++ src/client/java/rh/maparthelper/gui/widget/DecorativeButtonWidget.java
@@ -1,15 +1,35 @@
 package rh.maparthelper.gui.widget;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
+import net.minecraft.client.gui.widget.PressableWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Colors;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.ColorHelper;
 
-public class DecorativeButtonWidget extends ButtonWidget {
+public class DecorativeButtonWidget extends PressableWidget {
+    private final PressAction onPress;
+    private Identifier texture;
+
     private DecorativeButtonWidget(int x, int y, int width, int height, Text message, PressAction pressAction) {
-        super(x, y, width, height, message, pressAction, DEFAULT_NARRATION_SUPPLIER);
+        super(x, y, width, height, message);
+        this.onPress = pressAction;
+    }
+
+    private DecorativeButtonWidget(int x, int y, int width, int height, Identifier texture, PressAction pressAction) {
+        super(x, y, width, height, Text.empty());
+        this.texture = texture;
+        this.onPress = pressAction;
+    }
+
+    @Override
+    public void onPress() {
+        this.onPress.onPress(this);
     }
 
     @Override
@@ -17,21 +37,50 @@ public class DecorativeButtonWidget extends ButtonWidget {
         MinecraftClient minecraftClient = MinecraftClient.getInstance();
         int i = ColorHelper.withAlpha(this.alpha, this.active ? Colors.WHITE : Colors.LIGHT_GRAY);
         this.drawMessage(context, minecraftClient.textRenderer, i);
+
+        if (texture != null) {
+            context.drawTexture(
+                    RenderPipelines.GUI_TEXTURED,
+                    texture,
+                    getX(), getY(),
+                    0, 0,
+                    width, height,
+                    width, height
+            );
+        }
     }
 
-    public static DecorativeButtonWidget.Builder builder(Text message, ButtonWidget.PressAction onPress) {
+    @Override
+    public void appendClickableNarrations(NarrationMessageBuilder builder) {
+        this.appendDefaultNarrations(builder);
+    }
+
+    public static DecorativeButtonWidget.Builder builder(Text message, PressAction onPress) {
         return new DecorativeButtonWidget.Builder(message, onPress);
     }
 
-    public static class Builder extends ButtonWidget.Builder {
+    public static DecorativeButtonWidget.Builder builder(Identifier texture, PressAction onPress) {
+        return new DecorativeButtonWidget.Builder(texture, onPress);
+    }
+
+    public static class Builder {
         private final Text message;
+        private final Identifier texture;
         private final PressAction onPress;
         private int width = 150;
         private int height = 20;
+        private int x = 0;
+        private int y = 0;
 
         public Builder(Text message, PressAction onPress) {
-            super(message, onPress);
             this.message = message;
+            this.texture = null;
+            this.onPress = onPress;
+        }
+
+        public Builder(Identifier texture, PressAction onPress) {
+            this.message = null;
+            this.texture = texture;
             this.onPress = onPress;
         }
 
@@ -46,8 +95,25 @@ public class DecorativeButtonWidget extends ButtonWidget {
             return this;
         }
 
-        public DecorativeButtonWidget build() {
-            return new DecorativeButtonWidget(0, 0, this.width, this.height, this.message, this.onPress);
+        public Builder position(int x, int y) {
+            this.x = x;
+            this.y = y;
+            return this;
         }
+
+        public Builder dimensions(int x, int y, int width, int height) {
+            return this.position(x, y).size(width, height);
+        }
+
+        public DecorativeButtonWidget build() {
+            if (message != null)
+                return new DecorativeButtonWidget(x, y, this.width, this.height, this.message, this.onPress);
+            return new DecorativeButtonWidget(x, y, this.width, this.height, this.texture, this.onPress);
+        }
+    }
+
+    @Environment(EnvType.CLIENT)
+    public interface PressAction {
+        void onPress(DecorativeButtonWidget button);
     }
 }
