9608571da9925d413151c92907ab02de2248fd5b
Anatoly Solomonov <solomonov010@mail.ru>
2025-11-06 14:30:57 +0400

    Resolved version compatibility problems

--- src/client/java/rh/maparthelper/util/RenderUtils.java
+++ src/client/java/rh/maparthelper/util/RenderUtils.java
@@ -1,38 +1,24 @@
 package rh.maparthelper.util;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.render.state.ItemGuiElementRenderState;
-import net.minecraft.client.render.item.KeyedItemRenderState;
-import net.minecraft.item.ItemDisplayContext;
 import net.minecraft.item.ItemStack;
-import org.joml.Matrix3x2f;
-import rh.maparthelper.render.ScaledItemGuiElementRenderer;
+import org.joml.Matrix3x2fStack;
+import rh.maparthelper.MapartHelper;
 
 public class RenderUtils {
     public static void renderItemStack(DrawContext context, ItemStack itemStack, String stateName, int x, int y, int width, int height) {
-        MinecraftClient client = MinecraftClient.getInstance();
-        KeyedItemRenderState keyedItemRenderState = new KeyedItemRenderState();
-        client.getItemModelManager().clearAndUpdate(
-                keyedItemRenderState,
-                itemStack,
-                ItemDisplayContext.GUI,
-                client.world,
-                client.player,
-                0
-        );
-        ItemGuiElementRenderState itemRenderState = new ItemGuiElementRenderState(
-                stateName,
-                new Matrix3x2f(context.getMatrices()),
-                keyedItemRenderState,
-                x, y,
-                context.scissorStack.peekLast()
-        );
-        context.state.addSpecialElement(new ScaledItemGuiElementRenderer.ScaledItemGuiElementRenderState(
-                itemRenderState,
-                x, y,
-                x + width, y + height,
-                width
-        ));
+        Matrix3x2fStack matrixStack = context.getMatrices();
+        matrixStack.pushMatrix();
+
+        if (MapartHelper.commonConfig.mapartEditor.scaleBlockWidgets) {
+            matrixStack.translate(x, y);
+            matrixStack.scale(width / 16f, height / 16f);
+            matrixStack.translate(-x, -y);
+        } else {
+            matrixStack.translate(4, 4);
+        }
+        context.drawItem(itemStack, x, y);
+
+        matrixStack.popMatrix();
     }
 }
