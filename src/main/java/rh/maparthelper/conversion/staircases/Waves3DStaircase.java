d92e072a4c90e2280d68ad7f194a95e8a4f4e4af
Anatoly Solomonov <solomonov010@mail.ru>
2026-02-12 14:37:23 +0400

    Improve Waves3DStaircase for images with transparency

--- src/main/java/rh/maparthelper/conversion/staircases/Waves3DStaircase.java
+++ src/main/java/rh/maparthelper/conversion/staircases/Waves3DStaircase.java
@@ -12,7 +12,7 @@ public class Waves3DStaircase implements IMapartStaircase {
         int height = colors.length + 1;
         int width = colors[0].length;
 
-        int[] negativeHeights = new int[width];
+        List<List<Integer>> negativeHeights = new ArrayList<>();
 
         List<List<Integer>> staircase = new ArrayList<>();
         for (int z = 0; z < height; z++) {
@@ -21,31 +21,45 @@ public class Waves3DStaircase implements IMapartStaircase {
         }
 
         for (int x = 0; x < width; x++) {
-            for (int z = height - 2; z >= 0; z--) {
-                if (z > 0 && colors[z - 1][x] == 0) continue;
-                int y = staircase.get(z + 1).get(x) + getHeightAlt(brights, x, z);
+            // Waves logic to modularly fix negative heights
+            negativeHeights.add(x, new ArrayList<>());
+            negativeHeights.get(x).add(0);
+            int waveNumber = 0;
+
+            for (int z = 1; z < height; z++) {
+                if (colors[z - 1][x] == 0) continue;
+                if (z > 1 && colors[z - 2][x] == 0) {
+                    waveNumber++; // Increase wave number if air blocks finished
+                    negativeHeights.get(x).add(0);
+                }
+                int y = staircase.get(z - 1).get(x);
+                if (z == 1 || colors[z - 2][x] != 0)
+                    y += getHeightAltFromNorthern(brights, x, z);
+
                 staircase.get(z).set(x, y);
-                negativeHeights[x] = Math.min(negativeHeights[x], y);
+                negativeHeights.get(x).set(waveNumber, Math.min(negativeHeights.get(x).get(waveNumber), y));
             }
         }
 
-        for (int z = 0; z < height; z++) {
-            for (int x = 0; x < width; x++) {
-                if (z == 0 && colors[z][x] == 0)
-                    staircase.get(z).set(x, 0);
-                else
-                    staircase.get(z).set(x, staircase.get(z).get(x) - negativeHeights[x]);
+        // Ascending blocks with negative heights, including aux blocks
+        for (int x = 0; x < width; x++) {
+            int waveNumber = 0;
+            for (int z = 0; z < height; z++) {
+                if (z > 0 && colors[z - 1][x] == 0) continue;
+                if (z > 2 && colors[z - 2][x] == 0) waveNumber++;
+                staircase.get(z).set(x, staircase.get(z).get(x) - negativeHeights.get(x).get(waveNumber));
             }
         }
 
         return staircase;
     }
 
-    private static int getHeightAlt(int[][] brights, int x, int z) {
-        if (z == brights.length) return 0;
-        int brightness = brights[z][x];
-        if (brightness == 0) return 1;
-        if (brightness == 2) return -1;
-        return 0;
+    // What the height diff comparing to [z - 1][x]
+    private static int getHeightAltFromNorthern(int[][] brights, int x, int staircaseZ) {
+        if (staircaseZ == 0) return 0;
+        int brightness = brights[staircaseZ - 1][x];
+        if (brightness == 0) return -1; // LOW
+        if (brightness == 2) return 1; // HIGH
+        return 0; // NORMAL
     }
 }
